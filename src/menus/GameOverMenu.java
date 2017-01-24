7630e4bae88c5610381cf7f6173511babab1dae0
Sullivan Ford <sullyjhf@hotmail.co.uk>
2017-01-24 22:24:17 +0000

    Show options on game over menu

--- src/menus/GameOverMenu.java
+++ src/menus/GameOverMenu.java
@@ -9,10 +9,13 @@ import java.awt.RenderingHints;
 import snake.Screen;
 
 public class GameOverMenu extends Menu {
+  private String[] options = { "RESTART", "BACK TO TITLE", "END GAME" };
+  private int selected = 0;
   private String title;
   private Color titleColor;
   private Font titleFont;
   private Font scoreFont;
+  private Font optionsFont;
   private String score;
 
   public GameOverMenu(int score) {
@@ -20,6 +23,7 @@ public class GameOverMenu extends Menu {
     this.titleColor = new Color(255, 255, 255);
     this.titleFont = new Font("Agency FB", Font.BOLD, 96);
     this.scoreFont = new Font("Agency FB", Font.PLAIN, 72);
+    this.optionsFont = new Font("Agency FB", Font.PLAIN, 48);
     this.score = String.valueOf(score);
   }
 
@@ -42,5 +46,27 @@ public class GameOverMenu extends Menu {
     g2d.drawString(title, titleX, titleY);
     g2d.setFont(scoreFont);
     g2d.drawString(score, scoreX, scoreY);
+
+    FontMetrics optionsMetrics = g2d.getFontMetrics(optionsFont);
+    g2d.setFont(optionsFont);
+    int optionsY = scoreY + 100;
+    int arrowWidth = optionsMetrics.stringWidth("-> ");
+    for (int i = 0; i < options.length; i++) {
+      String option = options[i];
+      String output = "";
+      int optionWidth = optionsMetrics.stringWidth(option);
+      int x = Screen.WIDTH / 2 - optionWidth / 2;
+      if (i == selected) {
+        output += "-> ";
+        x -= arrowWidth;
+      }
+      output += option;
+      g2d.drawString(output, x, optionsY + i * 50);
+    }
+  }
+
+  @Override
+  public void tick(boolean[] keys) {
+
   }
 }
