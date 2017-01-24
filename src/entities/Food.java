34195ec43c706c34423833168c59d66b2bf4e77f
Sullivan Ford <sullyjhf@hotmail.co.uk>
2017-01-24 22:59:04 +0000

    Choose a random (brightish) colour for food

--- src/entities/Food.java
+++ src/entities/Food.java
@@ -2,6 +2,9 @@ package entities;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 import snake.Screen;
@@ -9,20 +12,23 @@ import snake.Screen;
 public class Food extends Entity {
   private final int SIZE = Snake.SIZE;
   private int x, y;
+  private Color color;
 
   public Food() {
     r = new Random();
+    color = chooseColour();
     update();
   }
 
   @Override
   public void draw(Graphics2D g2d) {
-    g2d.setColor(Color.RED);
+    g2d.setColor(color);
     g2d.fillOval(cellX, cellY, SIZE, SIZE);
   }
 
   @Override
   public void update() {
+    color = chooseColour();
     x = r.nextInt(Screen.WIDTH);
     y = r.nextInt(Screen.HEIGHT);
     cellX = x / SIZE * SIZE;
@@ -40,5 +46,20 @@ public class Food extends Entity {
     if (collide(Snake.self)) {
       update();
     }
+  }
+
+  private Color chooseColour() {
+    float a = r.nextFloat() / 4 + 0.7f;
+    float b = r.nextFloat() / 4 + 0.1f;
+    float c = r.nextFloat() / 4 + 0.1f;
+    List<Float> colours = new ArrayList<Float>();
+    colours.add(a);
+    colours.add(b);
+    colours.add(c);
+    Collections.shuffle(colours);
+    return new Color(
+        colours.get(0),
+        colours.get(1),
+        colours.get(2));
   }
 }
