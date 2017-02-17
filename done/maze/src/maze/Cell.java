241e6c8844d0e3c441a02c9ea5488a6749fdfe8c
Sullivan Ford <sullyjhf@hotmail.co.uk>
2017-02-17 12:06:06 +0000

    Add finished maze example

--- done/maze/src/maze/Cell.java
+++ done/maze/src/maze/Cell.java
@@ -1,26 +1,37 @@
 package maze;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.Line2D;
 
 public class Cell {
-  private int i, j;
+  public int i, j;
   static final int SIZE = 40;
   private Line2D top, right, bottom, left;
+  public boolean drawTop, drawRight, drawBottom, drawLeft;
+
+  public boolean visited = false;
 
   public Cell(int i, int j) {
     this.i = i;
     this.j = j;
     this.top = new Line2D.Double(i * SIZE, j * SIZE, (i + 1) * SIZE, j * SIZE);
     this.right = new Line2D.Double((i + 1) * SIZE, j * SIZE, (i + 1) * SIZE, (j + 1) * SIZE);
-    this.bottom = new Line2D.Double((i + 1) * SIZE, (j + 1) * SIZE, i * SIZE, (j+1) * SIZE);
+    this.bottom = new Line2D.Double((i + 1) * SIZE, (j + 1) * SIZE, i * SIZE, (j + 1) * SIZE);
     this.left = new Line2D.Double(i * SIZE, (j + 1) * SIZE, i * SIZE, j * SIZE);
+    drawTop = drawRight = drawBottom = drawLeft = true;
   }
 
-  public void draw(Graphics2D g2d) {
-    g2d.draw(this.top);
-    g2d.draw(this.right);
-    g2d.draw(this.bottom);
-    g2d.draw(this.left);
+  public void draw(Graphics2D g2d, Color color) {
+    g2d.setColor(color);
+    if (drawTop) g2d.draw(this.top);
+    if (drawRight) g2d.draw(this.right);
+    if (drawBottom) g2d.draw(this.bottom);
+    if (drawLeft) g2d.draw(this.left);
+  }
+
+  public void fill(Graphics2D g2d, Color color) {
+    g2d.setColor(color);
+    g2d.fillRect(i * SIZE, j * SIZE, SIZE, SIZE);
   }
 }
--- done/maze/src/maze/Screen.java
+++ done/maze/src/maze/Screen.java
@@ -4,6 +4,9 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Stack;
 
 import javax.swing.JPanel;
 
@@ -16,29 +19,85 @@ public class Screen extends JPanel {
 
   private Cell[] grid;
 
+  private Cell current;
+  private Cell end;
+
+  private Cell fork;
+
+  private int biggest = 0;
+
+  private Stack<Cell> stack;
+  private Stack<Cell> solution;
+
   public Screen() {
     setPreferredSize(new Dimension(WIDTH, HEIGHT));
     setBackground(Color.GRAY);
 
     grid = new Cell[ROWS * COLS];
-    int index = 0;
-    for(int i = 0; i < COLS; i++) {
-      for(int j = 0; j < ROWS; j++) {
-        grid[index] = new Cell(i, j);
-        ++index;
+    for (int i = 0; i < COLS; i++) {
+      for (int j = 0; j < ROWS; j++) {
+        grid[i + j * ROWS] = new Cell(i, j);
       }
     }
+
+    current = grid[0];
+    end = grid[grid.length - 1];
+    fork = null;
+    current.visited = true;
+
+    stack = new Stack<Cell>();
+    solution = new Stack<Cell>();
   }
 
   private void draw(Graphics g) {
     Graphics2D g2d = (Graphics2D) g.create();
-    for(Cell c : grid) {
-      c.draw(g2d);
+    for (Cell c : grid) {
+      if (c.visited) {
+        c.fill(g2d, Color.DARK_GRAY);
+      }
+      if (stack.contains(c)) c.fill(g2d, Color.LIGHT_GRAY);
+      if (solution.contains(c)) c.fill(g2d, new Color(200, 67, 170));
+      c.draw(g2d, Color.WHITE);
     }
+    end.fill(g2d, Color.RED);
+    current.fill(g2d, Color.GREEN);
   }
 
   public void tick() {
+    ArrayList<Cell> curNeighbours = getNeighbours(current);
+    if (curNeighbours.size() > 0) {
+      Cell next = pickCell(curNeighbours);
+      stack.push(current);
+      if (stack.size() > biggest) {
+        biggest = stack.size();
+        end = next;
+        fork = null;
+        solution.clear();
+      }
+      next.visited = true;
+      removeWalls(current, next);
+      current = next;
+    } else if (!stack.isEmpty()) {
+      current = stack.pop();
+      fillSolution();
+    }
+  }
 
+  private void fillSolution() {
+    if (fork == null && getNeighbours(current).size() == 0) {
+      solution.push(current);
+    } else if (fork == null && getNeighbours(current).size() > 0) {
+      fork = current;
+    } else if (fork != null && current.equals(fork)) {
+      solution.push(current);
+      fork = null;
+    }
+  }
+
+  private Cell pickCell(ArrayList<Cell> curNeighbours) {
+    Random r = new Random();
+    int index = r.nextInt(curNeighbours.size());
+    return curNeighbours.get(index);
   }
 
   public void render() {
@@ -49,5 +108,53 @@ public class Screen extends JPanel {
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
     draw(g);
+  }
+
+  private ArrayList<Cell> getNeighbours(Cell cell) {
+    Cell topCell = getUnvisitedCell(cell.i, cell.j - 1);
+    Cell rightCell = getUnvisitedCell(cell.i + 1, cell.j);
+    Cell bottomCell = getUnvisitedCell(cell.i, cell.j + 1);
+    Cell leftCell = getUnvisitedCell(cell.i - 1, cell.j);
+
+    ArrayList<Cell> neighbours = new ArrayList<Cell>();
+
+    if (topCell != null) neighbours.add(topCell);
+    if (rightCell != null) neighbours.add(rightCell);
+    if (bottomCell != null) neighbours.add(bottomCell);
+    if (leftCell != null) neighbours.add(leftCell);
+
+    return neighbours;
+  }
+
+  private Cell getCell(int x, int y) {
+    int index = x + y * ROWS;
+    if (x < 0 || x >= COLS || y < 0 || y >= ROWS) return null;
+    return grid[index];
+  }
+
+  private Cell getUnvisitedCell(int x, int y) {
+    Cell cell = getCell(x, y);
+    if (cell == null || cell.visited) return null;
+    return cell;
+  }
+
+  private void removeWalls(Cell a, Cell b) {
+    int xDiff = b.i - a.i;
+    int yDiff = b.j - a.j;
+
+    if (xDiff == 1) {
+      a.drawRight = false;
+      b.drawLeft = false;
+    } else if (xDiff == -1) {
+      a.drawLeft = false;
+      b.drawRight = false;
+    }
+    if (yDiff == 1) {
+      a.drawBottom = false;
+      b.drawTop = false;
+    } else if (yDiff == -1) {
+      a.drawTop = false;
+      b.drawBottom = false;
+    }
   }
 }
