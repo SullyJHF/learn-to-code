ea05214716ffcba0ca6d4553e1458552478dfe63
Sullivan Ford <sullyjhf@hotmail.co.uk>
2017-02-21 21:28:56 +0000

    Make ball bounce off ceiling and floor

--- done/pong/src/pong/Ball.java
+++ done/pong/src/pong/Ball.java
@@ -9,12 +9,15 @@ public class Ball {
   private float angle;
   private float size;
 
+  private float speed;
+
   private Color color;
 
   public Ball() {
     Random r = new Random();
     this.angle = (float) (r.nextFloat() * 2 * Math.PI);
     this.size = 13;
+    this.speed = 2;
     this.x = Screen.WIDTH / 2 - this.size / 2;
     this.y = Screen.HEIGHT / 2 - this.size / 2;
     this.color = Color.WHITE;
@@ -30,6 +33,21 @@ public class Ball {
   }
 
   public void tick() {
+    if (this.x + this.size > Screen.WIDTH) {
+      // game over, one point to left
+    } else if (this.x < 0) {
+      // game over, one point to right
+    }
 
+    if (this.y + this.size > Screen.HEIGHT || this.y < 0) {
+      this.angle = (float) (Math.PI - this.angle);
+    }
+
+    float d = this.speed;
+    float dx = (float) (d * Math.sin(this.angle));
+    float dy = (float) (d * Math.cos(this.angle));
+
+    this.x += dx;
+    this.y += dy;
   }
 }
