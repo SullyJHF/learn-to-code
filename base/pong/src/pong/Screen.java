79e6599bfb3fe7f6b4ced0e8086f07721877122c
Sullivan Ford <sullyjhf@hotmail.co.uk>
2017-02-21 19:40:41 +0000

    Add pong base

---
+++ base/pong/src/pong/Screen.java
@@ -0,0 +1,36 @@
+package pong;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+public class Screen extends JPanel {
+  private static final int WIDTH = 800;
+  private static final int HEIGHT = 600;
+
+  public Screen() {
+    setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    setBackground(Color.BLACK);
+  }
+
+  private void draw(Graphics g) {
+    Graphics2D g2d = (Graphics2D) g.create();
+  }
+
+  public void tick(boolean[] keys) {
+
+  }
+
+  public void render() {
+    repaint();
+  }
+
+  @Override
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    draw(g);
+  }
+}
